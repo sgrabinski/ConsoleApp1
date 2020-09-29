49ddb8fd3c64ce9fbc15f3853163fdbd4f2f0a1a
sgrabinski <sgrabinski@STINGER>
2020-09-29 17:24:46 +0200

    Added class TestRule PB.DNUP

---
+++ ConsoleApp1/TestRule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class TestRule
+    {
+        //test rule PB.DNUP
+        public double Foo(string s)
+        {
+            double doubleVal = Double.Parse(s);    // Violation
+            return doubleVal;
+        }
+    }
+}
