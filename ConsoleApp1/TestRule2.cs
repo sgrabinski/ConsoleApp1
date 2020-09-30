fc862708dba76801413166c5c065f0486ae33f55
sgrabinski <sgrabinski@STINGER>
2020-09-30 11:05:18 +0200

    Updade 2020-09-30 11:04

--- ConsoleApp1/TestRule2.cs
+++ ConsoleApp1/TestRule2.cs
@@ -2,24 +2,27 @@
 
 namespace ConsoleApp1
 {
-    class TestRule
+    class TestRule2
     {
         //test rule PB.DNUP
-        public double Foo(string s)
+        public double Foo1(string s)
         {
-            //
+            // 2020-09-30 11:04
             double doubleVal = Double.Parse(s);    // Violation
             return doubleVal;
+            //
         }
         public double Foo2(string s)
         {
             double doubleVal2 = Double.Parse(s);    // Violation
             return doubleVal2;
             //
+            // 2020-09-30 11:04
         }
         public double Foo3(string s)
         {
             //
+            // 2020-09-30 11:04
             double doubleVal3 = Double.Parse(s);    // Violation
             return doubleVal3;
         }
