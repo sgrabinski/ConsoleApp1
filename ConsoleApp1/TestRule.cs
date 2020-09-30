93950a2ee31b303c93602cec5bbb760ef578b702
sgrabinski <sgrabinski@STINGER>
2020-09-30 09:12:00 +0200

    Send TestRule.cs

--- ConsoleApp1/TestRule.cs
+++ ConsoleApp1/TestRule.cs
@@ -7,8 +7,21 @@ namespace ConsoleApp1
         //test rule PB.DNUP
         public double Foo(string s)
         {
+            //
             double doubleVal = Double.Parse(s);    // Violation
             return doubleVal;
+        }
+        public double Foo2(string s)
+        {
+            double doubleVal2 = Double.Parse(s);    // Violation
+            return doubleVal2;
+            //
+        }
+        public double Foo3(string s)
+        {
+            //
+            double doubleVal3 = Double.Parse(s);    // Violation
+            return doubleVal3;
         }
     }
 }
